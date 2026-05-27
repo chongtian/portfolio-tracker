3d78be45aa337dc024d9a53a82a8c2852be00faa
Chongtian Gao <chongtiangao@gmail.com>
2026-05-27 10:52:05 -0500

    Stopped calling OptionWatch api

--- service/src/shared/utils/getMarketPrice.ts
+++ service/src/shared/utils/getMarketPrice.ts
@@ -1,8 +1,8 @@
 import { MarketPrice } from "@shared/models/marketPrice";
-import { getStockFundPriceFromAlphaVantage } from "./alphaVantageApiHelper";
+// import { getStockFundPriceFromAlphaVantage } from "./alphaVantageApiHelper";
 import { parseOptionContract } from "./parseOptionContract";
 import { getFundStockHistoricalPrice, getOptionContractPrice, getStockPrice } from "./marketDataApiHelper";
-import { getOptionContractPriceFromOptionWatch } from "./getOptionContractPrice";
+// import { getOptionContractPriceFromOptionWatch } from "./getOptionContractPrice";
 
 export const getCurrentMarketPrice = async (instrumentId: string): Promise<MarketPrice> => {
 
@@ -14,16 +14,18 @@ export const getCurrentMarketPrice = async (instrumentId: string): Promise<Marke
 
     if (isOption) {
 
-        // Try to get option price from OptionWatch.io first
-        const optionPrice = await getOptionContractPriceFromOptionWatch(instrumentId);
+        // // Try to get option price from OptionWatch.io first
+        // const optionPrice = await getOptionContractPriceFromOptionWatch(instrumentId);
 
-        if (!optionPrice.success) {
-            // fall back to Market Data API to get price for option contract
-            // the price is 1-day older
-            return await getOptionContractPrice(optionContract, marketDataApiKey);
-        } else {
-            return optionPrice;
-        }
+        // if (!optionPrice.success) {
+        //     // fall back to Market Data API to get price for option contract
+        //     // the price is 1-day older
+        //     return await getOptionContractPrice(optionContract, marketDataApiKey);
+        // } else {
+        //     return optionPrice;
+        // }
+
+        return await getOptionContractPrice(optionContract, marketDataApiKey);
 
     } else {
 
